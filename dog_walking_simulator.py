fad9e9827dd414bfa955219a6b54bb06e8f271d3
David12Siska <125990271+David12Siska@users.noreply.github.com>
2023-04-19 13:38:40 +0300

    Create dog_walking_simulator.py

---
+++ dog_walking_simulator.py
@@ -0,0 +1,15 @@
+# Название файла: dog_walking_simulator.py
+
+import time
+
+print("Приготовьтесь выгуливать свою собаку.")
+time.sleep(1)
+
+for i in range(3):
+    print("Идите с собакой на улицу.")
+    time.sleep(2)
+
+    print("Позвольте собаке понюхать окружающую местность.")
+    time.sleep(2)
+
+    print("Бегайте
